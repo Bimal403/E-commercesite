2f7a454db53651102139697d0493ad49926d2e18
BimalTimsina <bimaltimsina4@gmail.com>
2023-09-01 22:13:32 +0545

    payment info modal

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ menuItems.forEach((item, index) => {
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
   });
 });
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
