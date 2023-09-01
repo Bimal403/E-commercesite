a67e14959adace63c7f52931fa7999d5f4847c0d
BimalTimsina <bimaltimsina4@gmail.com>
2023-09-01 23:02:25 +0545

    new changes made

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ menuItems.forEach((item, index) => {
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
   });
 });
-const productButton = document.querySelector(".productButton");
+const productButton = document.querySelector(".buyButton");
+const productButton1 = document.querySelector(".buyButton1");
+const productButton2 = document.querySelector(".buyButton2");
+const productButton3 = document.querySelector(".buyButton3");
+const productButton4 = document.querySelector(".buyButton4");
 const payment = document.querySelector(".payment");
 const close = document.querySelector(".close");
 
@@ -14,4 +18,16 @@ productButton.addEventListener("click", () => {
 });
 close.addEventListener("click", () => {
   payment.style.display = "none";
+});
+productButton1.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+productButton2.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+productButton3.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+productButton4.addEventListener("click", () => {
+  payment.style.display = "flex";
 });
